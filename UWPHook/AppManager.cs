e57a53af9f0d12be907da23637deb636c65e73f1
stevealexandre <mail@stevealexandre.fr>
2022-10-09 04:09:30 +0200

    refactor getprocess for checking running and bring process

--- UWPHook/AppManager.cs
+++ UWPHook/AppManager.cs
@@ -20,10 +20,8 @@ namespace UWPHook
     /// </summary>
     static class AppManager
     {
-        private static int runningProcessId;
-        private static bool isLauncherProcess;
+        private static int? runningProcessId;
         private static string executablePath;
-        private static string executableName;
 
         /// <summary>
         /// Launch a UWP App using a ApplicationActivationManager and sets a internal id to launched proccess id
@@ -36,6 +34,7 @@ namespace UWPHook
             // 1 is the aumid,
             // 2 is the executable, the rest are extras
             string aumid = args[1];
+            executablePath = args[2].Contains("/") ? args[2].Replace('/', '\\') : args[2];
             Log.Verbose("Arguments => " + String.Join("/", args));
 
             string extra_args = String.Join(" ", args.Skip(3).Take(args.Length - 3).Select(eachElement => eachElement.Clone()).ToArray());
@@ -48,7 +47,6 @@ namespace UWPHook
                 processStartInfo.FileName = @"shell:appsFolder\" + aumid;
                 process.StartInfo = processStartInfo;
                 process.Start();
-                runningProcessId = process.Id;
 
                 //Bring the launched app to the foreground, this fixes in-home streaming
                 BringProcess();
@@ -66,33 +64,8 @@ namespace UWPHook
         /// <returns>True if the perviously launched app is running, false otherwise</returns>
         public static Boolean IsRunning()
         {
-            bool secondCheck = false;
-           
-            do
-            {
-                // Handle process running by some launcher by checking their executable path and name
-                var processes = GetProcess();
-                foreach (var process in processes)
-                {
-                    string executableFile = executablePath.Contains('\\') ? executablePath.Substring(executablePath.LastIndexOf('\\') + 1) : executablePath;
-                    Log.Verbose("Process " + process.Value.Path + " contains " + executablePath + " ? : " + process.Value.Path.Contains(executablePath).ToString());
-                    Log.Verbose("Process " + process.Key + " contains " + executableFile + " ? : " + process.Key.Contains(executableFile).ToString());
-                    if (process.Value.Path.Contains(executablePath) || process.Key.Contains(executableFile))
-                    {
-                        return true;
-                    }
-                }
-
-                secondCheck = !secondCheck;
-
-                if (secondCheck)
-                {
-                    Log.Debug("Process has not been found. Last chance to find it !");
-                    Thread.Sleep(Settings.Default.Seconds * 5000);
-                }
-            } while (secondCheck);
-
-            Log.Debug("Process is not running.");
+            runningProcessId = getProcess();
+            if (runningProcessId != null && runningProcessId > 0) return true;
             return false;
         }
 
@@ -173,69 +146,54 @@ namespace UWPHook
             const int SW_SHOWDEFAULT = 10;
             */
 
-            var arrProcesses = Process.GetProcessById(runningProcessId);
-
-            // get the window handle
-            IntPtr hWnd = arrProcesses.MainWindowHandle;
-
-            // if iconic, we need to restore the window
-            if (IsIconic(hWnd))
+            if (runningProcessId != null && runningProcessId > 0)
             {
-                ShowWindowAsync(hWnd, 3);
+                Process process = Process.GetProcessById((int)runningProcessId);
+
+                // get the window handle
+                IntPtr hWnd = process.MainWindowHandle;
+
+                // if iconic, we need to restore the window
+                if (IsIconic(hWnd))
+                {
+                    ShowWindowAsync(hWnd, 3);
+                }
+
+                // bring it to the foreground
+                SetForegroundWindow(hWnd);
             }
-
-            // bring it to the foreground
-            SetForegroundWindow(hWnd);
-
         }
 
-    }
+        private static int? getProcess()
+        {
+            bool secondCheck = false;
 
-    public enum ActivateOptions
-    {
-        None = 0x00000000,  // No flags set
-        DesignMode = 0x00000001,  // The application is being activated for design mode, and thus will not be able to
-                                  // to create an immersive window. Window creation must be done by design tools which
-                                  // load the necessary components by communicating with a designer-specified service on
-                                  // the site chain established on the activation manager.  The splash screen normally
-                                  // shown when an application is activated will also not appear.  Most activations
-                                  // will not use this flag.
-        NoErrorUI = 0x00000002,  // Do not show an error dialog if the app fails to activate.
-        NoSplashScreen = 0x00000004,  // Do not show the splash screen when activating the app.
-    }
+            do
+            {
+                // Handle process running by some launcher by checking their executable path and name
+                var processes = GetProcess();
+                foreach (var process in processes)
+                {
+                    string executableFile = executablePath.Contains('\\') ? executablePath.Substring(executablePath.LastIndexOf('\\') + 1) : executablePath;
+                    Log.Verbose("Process " + process.Value.Path + " contains " + executablePath + " ? : " + process.Value.Path.Contains(executablePath).ToString());
+                    Log.Verbose("Process " + process.Key + " contains " + executableFile + " ? : " + process.Key.Contains(executableFile).ToString());
+                    if (process.Value.Path.Contains(executablePath) || process.Key.Contains(executableFile))
+                    {
+                        return process.Value.Pid;
+                    }
+                }
 
-    [ComImport, Guid("2e941141-7f97-4756-ba1d-9decde894a3d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    interface IApplicationActivationManager
-    {
-        // Activates the specified immersive application for the "Launch" contract, passing the provided arguments
-        // string into the application.  Callers can obtain the process Id of the application instance fulfilling this contract.
-        IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
-        IntPtr ActivateForFile([In] String appUserModelId, [In] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)] /*IShellItemArray* */ IShellItemArray itemArray, [In] String verb, [Out] out UInt32 processId);
-        IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
-    }
+                secondCheck = !secondCheck;
 
-    [ComImport, Guid("45BA127D-10A8-46EA-8AB7-56EA9078943C")]//Application Activation Manager
-    class ApplicationActivationManager : IApplicationActivationManager
-    {
-        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)/*, PreserveSig*/]
-        public extern IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
-        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        public extern IntPtr ActivateForFile([In] String appUserModelId, [In] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)]  /*IShellItemArray* */ IShellItemArray itemArray, [In] String verb, [Out] out UInt32 processId);
-        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        public extern IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
-    }
+                if (secondCheck)
+                {
+                    Log.Debug("Process has not been found. Last chance to find it !");
+                    Thread.Sleep(Settings.Default.Seconds * 5000);
+                }
+            } while (secondCheck);
 
-    [ComImport]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    [Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
-    interface IShellItem
-    {
-    }
-
-    [ComImport]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    [Guid("b63ea76d-1f85-456f-a19c-48159efa858b")]
-    interface IShellItemArray
-    {
+            Log.Debug("Process is not running.");
+            return null;
+        }
     }
 }
